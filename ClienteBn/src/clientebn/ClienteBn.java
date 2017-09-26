c4bc68f566628a18459da65ed027d66e0f037dc5
Corona IFPE <corona@LABS.GARANHUNS.IFPE>
2017-09-26 11:09:39 -0300

    adicionando cliente do jogo

--- ClienteBn/src/clientebn/ClienteBn.java
+++ ClienteBn/src/clientebn/ClienteBn.java
@@ -8,6 +8,7 @@ package clientebn;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -23,24 +24,51 @@ public class ClienteBn {
      */
     public static void main(String[] args) throws IOException {
         
-        Socket cliente = new Socket("127.0.0.1", 1234);
+    }
+    
+    private String host;
+    private int porta;
+
+    public ClienteBn(String host, int porta) {
+        this.host = host;
+        this.porta = porta;
+    }
+    
+    public void execulta() throws IOException{
+        Socket cliente = new Socket(host, porta);
+        System.out.println("Cliente conecetado ao servidor!");
         
-        ObjectInputStream resultado = new ObjectInputStream(cliente.getInputStream());
-        ObjectOutputStream dados = new ObjectOutputStream(cliente.getOutputStream());
-        
+        Recebedor recebedor = new Recebedor(cliente.getInputStream());
+        new Thread(recebedor).start();
         
         Scanner teclado = new Scanner(System.in);
+        PrintStream saida = new PrintStream(cliente.getOutputStream());
         
-        String tabela = dados.read();
-        
-        while(!teclado.equals("sair")){
-            
-            resultado.close();
-            dados.close();
-            cliente.close();
-            
+        while(teclado.hasNextLine()){
+            saida.println(teclado.nextLine());
         }
-                
+        
+        saida.close();
+        teclado.close();
+        cliente.close();
     }
     
 }
+
+
+//        
+//        ObjectInputStream resultado = new ObjectInputStream(cliente.getInputStream());
+//        ObjectOutputStream dados = new ObjectOutputStream(cliente.getOutputStream());
+//        
+//        
+//        Scanner teclado = new Scanner(System.in);
+//        
+//        String tabela = dados.read();
+//        
+//        while(!teclado.equals("sair")){
+//            
+//            resultado.close();
+//            dados.close();
+//            cliente.close();
+//            
+//        }
---
+++ ClienteBn/src/clientebn/Recebedor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientebn;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author corona
+ */
+public class Recebedor implements Runnable{
+
+    private InputStream servidor;
+
+    public Recebedor(InputStream servidor) {
+        this.servidor = servidor;
+    }
+
+    public void run() {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextLine()) {
+            System.out.println(s.nextLine());
+        }
+    }
+}
